42bc8a2cc60bb69a23b45822f5e74c946c29250d
brits <joseluizbrits@gmail.com>
2023-09-19 18:46:05 -0300

    set route to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import GlobalStyle from './styles/GlobalStyles';
-import Home from './pages/Home/Home';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyles";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/magic-box" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
